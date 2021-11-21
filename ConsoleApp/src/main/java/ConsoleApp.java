aedb3543649e0548dafc61000d3d7d8ba7e38ab3
K0TM7 <72389295+K0TM7@users.noreply.github.com>
2021-11-21 13:09:16 -0500

    Delete ConsoleApp/src/main/java directory

--- ConsoleApp/src/main/java/ConsoleApp.java
+++
@@ -1,7 +0,0 @@
-public class ConsoleApp {
-
-    public static void main(String[] args) {
-        String appName = CommonUtility.getAppName();
-        System.out.println("Welcome to " + appName);
-    }
-}
